a323baa9534119975dc11097ad1025feedc4948c
ajaism <ajeem.pathan@gmail.com>
2021-12-26 20:12:34 +0530

    simple Break example

---
+++ breakex1.py
@@ -0,0 +1,10 @@
+#breakex1.py
+s = "PYTHON"
+for v in s:
+   if(v=="""H"""):
+        break
+   else:
+       print(v)
+else:
+      print("Else part of for loop")
+print("other statement in program")
